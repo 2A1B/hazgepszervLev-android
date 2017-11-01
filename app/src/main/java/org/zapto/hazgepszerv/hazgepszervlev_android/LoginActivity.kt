6d5c62522c58aa1d94edaf20a5c370e1801234ac
madar94 <madar94@gmail.com>
2017-11-01 16:16:36 +0100

    Progress dialog fix

--- app/src/main/java/org/zapto/hazgepszerv/hazgepszervlev_android/LoginActivity.kt
+++ app/src/main/java/org/zapto/hazgepszerv/hazgepszervlev_android/LoginActivity.kt
@@ -1,44 +1,44 @@
 package org.zapto.hazgepszerv.hazgepszervlev_android
 
-import android.app.ProgressDialog
 import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.Toast
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import org.jetbrains.anko.indeterminateProgressDialog
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
-    var fbAuth = FirebaseAuth.getInstance()
+    private var fbAuth = FirebaseAuth.getInstance()!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_login)
 
-        var btnLogin = findViewById<Button>(R.id.btn_login)
+        val btnLogin = findViewById<Button>(R.id.btn_login)
+
+        if (fbAuth.currentUser != null) {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
 
         btnLogin.setOnClickListener({_ -> login()})
     }
 
-    fun login() {
-        //indeterminateProgressDialog("Bejelentkezés").show()
+    private fun login() {
+        val dialog = indeterminateProgressDialog("Bejelentkezés...")
+
+        dialog.show()
 
         val email = input_email.text.toString()
         val password = input_password.text.toString()
 
-        Toast.makeText(baseContext, email, Toast.LENGTH_LONG).show()
-        Toast.makeText(baseContext, password, Toast.LENGTH_LONG).show()
-
-
-        fbAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, OnCompleteListener<AuthResult> { task ->
+        fbAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, { task ->
+            dialog.hide()
             if (task.isSuccessful) {
-                indeterminateProgressDialog("Bejelentkezés").hide()
                 onLoginSuccess()
             } else {
                 onLoginFailed()
@@ -46,16 +46,16 @@ class LoginActivity : AppCompatActivity() {
         })
     }
 
-    fun onLoginSuccess() {
-        btn_login.setEnabled(true)
+    private fun onLoginSuccess() {
+        btn_login.isEnabled = true
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
         finish()
     }
 
-    fun onLoginFailed() {
+    private fun onLoginFailed() {
         Toast.makeText(baseContext, "A bejelentkezés sikertelen.", Toast.LENGTH_LONG).show()
-        btn_login.setEnabled(true)
+        btn_login.isEnabled = true
     }
 
 }
